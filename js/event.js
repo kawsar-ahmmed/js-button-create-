0f1f8a1a04b200319d19ed05e7a2289c1782d85d
kawsar91221 <designer.kawsar2018@gmail.com>
2021-08-13 04:59:32 +0600

    new-items

--- js/event.js
+++ js/event.js
@@ -18,3 +18,26 @@ blueButton.onclick = makeBlue;
 function makeBlue (){
     document.body.style.background = 'blue';
 }
+
+// Different ways to use addEventListener on a button 
+const nativeButton = document.getElementById('native-button');
+
+nativeButton.onclick = function(){
+    document.body.style.background = '#000'
+}
+
+// Different ways to use addEventListener on a button 
+const narroGold = document.getElementById('narro-button');
+narroGold.addEventListener('click', makeGolden);
+function makeGolden (){
+    document.body.style.background = '#1e7aca'
+}
+
+// Different ways to use addEventListener on a button another way
+const hotPink = document.getElementById('hotPink-button');
+// console.log(hotPink);
+hotPink.addEventListener('click', function makePink (){
+    document.body.style.background = 'pink'
+})
+
+
